cdc9e96a0d20e0b09aa52cc81dc9d53fb30f694d
muradxkira <mahfujur797@gmail.com>
2023-01-01 09:04:12 +0600

    polished

--- index.js
+++ index.js
@@ -51,7 +51,12 @@ async function run() {
 
         app.get('/posts', async (req, res) => {
             const query = {};
-            const result = await postsCollection.find(query).toArray();
+            const result = await postsCollection.find(query).sort({ time: -1 }).toArray();
+            res.send(result);
+        });
+        app.get('/home', async (req, res) => {
+            const query = {};
+            const result = await postsCollection.find(query).limit(3).sort({ like: -1 }).toArray();
             res.send(result);
         });
 
@@ -61,6 +66,21 @@ async function run() {
             const result = await postsCollection.findOne(query);
             res.send(result);
         });
+
+        app.put('/status/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const filter = { _id: ObjectId(id) };
+            const currentLike = req.body;
+            const option = { upsert: true }
+            const updatedLikes = {
+                $set: {
+                    like: currentLike.like,
+                }
+            }
+            const result = await postsCollection.updateOne(filter, updatedLikes, option);
+            res.send(result);
+        })
 
         //comments section 
         app.post('/comments', async (req, res) => {
@@ -78,7 +98,7 @@ async function run() {
         app.get('/comments/:postid', async (req, res) => {
             const postId = req.params.postid
             const query = {postId: postId};
-            const result = await commentsCollection.find(query).toArray();
+            const result = await commentsCollection.find(query).sort({ time: -1 }).toArray();
             res.send(result);
         });
 
@@ -96,6 +116,19 @@ async function run() {
             const result = await usersCollection.findOne(query);
             res.send(result);
         });
+
+        // app.post('/postlike', async (req, res) => {
+        //     const users = req.body;
+        //     const result = await usersCollection.insertOne(users);
+        //     res.send(result);
+        // });
+
+        // app.get('/postlike', async (req, res) => {
+        //     const email = req.params.email;
+        //     const query = {email: email};
+        //     const result = await usersCollection.findOne(query);
+        //     res.send(result);
+        // });
 
        
 
